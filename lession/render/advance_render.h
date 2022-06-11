f6ffb29245905add412726378059e1877f2003d0
王波 <wangbo01@jidu002861.local>
2022-06-12 01:06:46 +0800

    [add]mix vertex attributes

--- lession/render/advance_render.h
+++ lession/render/advance_render.h
@@ -32,6 +32,13 @@ private:
             0.5f, -0.5f, 0.0f,
             0.0f, 0.5f, 0.0f,
     };
+
+    constexpr static float verticeAndColors[] = {
+            // positions         // colors
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+    };
 };
 
 
--- resources/shaders/advance/fShader.fs
+++ resources/shaders/advance/fShader.fs
@@ -1,8 +1,8 @@
 #version 330 core
 out vec4 FragColor;
-uniform vec4 outColor;
+in vec3 outColor;
 
 void main()
 {
-    FragColor = outColor;
+    FragColor = vec4(outColor, 1.0f);
 }
