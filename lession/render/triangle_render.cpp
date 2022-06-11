9cf353c2bb0b0adad8347d9a9beaba2693527bab
王波 <wangbo01@jidu002861.local>
2022-06-11 20:48:44 +0800

    [add]draw rectangle

--- lession/render/triangle_render.cpp
+++ lession/render/triangle_render.cpp
@@ -6,6 +6,8 @@
 
 
 constexpr float TriangleRender::vertices[];
+constexpr float TriangleRender::rectangle_vertices[];
+constexpr int TriangleRender::indices[];
 
 void TriangleRender::onInit() {
     shader = new Shader(vShaderPath, fShaderPath);
@@ -19,7 +21,16 @@ void TriangleRender::onInit() {
 
     //bind VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    //draw triangle
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //draw rectangle
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle_vertices), rectangle_vertices, GL_STATIC_DRAW);
+
+    //bind EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //设置顶点属性指针
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof (float ), (void*)0);
@@ -27,14 +38,24 @@ void TriangleRender::onInit() {
 
     //release
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     glBindVertexArray(0);
 
+    shader->use();
 }
 
 void TriangleRender::onDraw() {
-    shader->use();
+
+// draw triangle
+//    glBindVertexArray(VAO);
+//    glDrawArrays(GL_TRIANGLES, 0, 3);
+//    //release
+//    glBindVertexArray(0);
+
+// draw rectangle
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
 
